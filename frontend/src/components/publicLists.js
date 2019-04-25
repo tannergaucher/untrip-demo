1c5e7e6129f9f5193b837c596ce0b2fee7d40614
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-04-25 12:22:37 +0800

    add page to render all public lists

--- frontend/src/components/publicLists.js
+++ frontend/src/components/publicLists.js
@@ -1,12 +1,43 @@
 import React from "react"
 import { Heading, Box } from "grommet"
+import gql from "graphql-tag"
+import { useQuery } from "react-apollo-hooks"
 
-// query app public lists from the api
+import Loading from "../components/loading"
+import Error from "../components/error"
+import Link from "../components/styles/link"
+
+const PUBLIC_LISTS_QUERY = gql`
+  query PUBLIC_LISTS_QUERY {
+    lists {
+      id
+      title
+    }
+  }
+`
 
 export default function publicLists() {
+  const { data, loading, error } = useQuery(PUBLIC_LISTS_QUERY)
+
+  if (loading) return <Loading />
+  if (error) return <Error error={error} />
+
   return (
     <Box margin="medium">
-      <Heading>Lists</Heading>
+      <Heading>Browse Lists</Heading>
+      {data.lists.map(list => (
+        <Box
+          key={list.id}
+          margin={{ vertical: "medium" }}
+          background="light-2"
+          pad="medium"
+          elevation="small"
+        >
+          <Link to={`/app/list/${list.id}`}>
+            <Heading level={3}>{list.title}</Heading>
+          </Link>
+        </Box>
+      ))}
     </Box>
   )
 }
